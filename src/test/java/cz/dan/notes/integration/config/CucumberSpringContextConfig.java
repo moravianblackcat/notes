a6fd7c90a0d1265a690f50cd284aaddcb3cbfcf3
Daniel Polách <polachdan@gmail.com>
2025-07-07 15:21:29 +0200

    Integration tests upgrade

--- src/test/java/cz/dan/notes/integration/config/CucumberSpringContextConfig.java
+++ src/test/java/cz/dan/notes/integration/config/CucumberSpringContextConfig.java
@@ -1,16 +1,10 @@
 package cz.dan.notes.integration.config;
 
 import cz.dan.integrationtests.http.HttpAutoConfiguration;
-import io.cucumber.java.DataTableType;
 import io.cucumber.spring.CucumberContextConfiguration;
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.HashMap;
-import java.util.Map;
 
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.DEFINED_PORT;
 
@@ -19,28 +13,5 @@ import static org.springframework.boot.test.context.SpringBootTest.WebEnvironmen
 @ActiveProfiles("integration-tests")
 @ImportAutoConfiguration({HttpAutoConfiguration.class})
 public class CucumberSpringContextConfig {
-
-    @DataTableType
-    public Map<String, Object> transform(Map<String, String> entry) {
-        Map<String, Object> transformedEntry = new HashMap<>();
-        DateTimeFormatter dateFormatter = DateTimeFormatter.ISO_LOCAL_DATE;
-
-        entry.forEach((key, value) -> {
-            if ("true".equalsIgnoreCase(value) || "false".equalsIgnoreCase(value)) {
-                transformedEntry.put(key, Boolean.parseBoolean(value));
-            } else if (value == null) {
-                transformedEntry.put(key, null);
-            } else if (value.matches("-?\\d+")) {
-                transformedEntry.put(key, Long.parseLong(value));
-            } else if (value.matches("-?\\d+\\.\\d+")) {
-                transformedEntry.put(key, Double.parseDouble(value));
-            } else if (value.matches("\\d{4}-\\d{2}-\\d{2}")) {
-                transformedEntry.put(key, LocalDate.parse(value, dateFormatter));
-            } else {
-                transformedEntry.put(key, value);
-            }
-        });
-        return transformedEntry;
-    }
 
 }
